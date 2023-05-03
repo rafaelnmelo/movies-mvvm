4a57026ffbd05367026b75ef8b364bc626c9faa3
Rafael Melo <rafaelnunesmelo@gmail.com>
2023-05-03 14:43:47 -0300

    Criando ação de toque e ajustes na organização

--- TrendingMovies/TrendingMovies/Controllers/MainViewController/MainViewController+TableView.swift
+++ TrendingMovies/TrendingMovies/Controllers/MainViewController/MainViewController+TableView.swift
@@ -14,25 +14,28 @@ extension MainViewController: UITableViewDelegate {
     func bindTableView() {
         /// Set delegate ao rx
         tableview.rx.setDelegate(self).disposed(by: bag)
+        
         /// Usando Rx para manejo do datasource
-        let dataSource = RxTableViewSectionedReloadDataSource<SectionModel<String,Movie>> { _,tableview,indexPath, item in
-            let cell = tableview.dequeueReusableCell(withIdentifier: MainMovieCell.identifier, for: indexPath) as! MainMovieCell
-            cell.setupCell(viewModel: MovieTableCellViewModel(movieData: item))
+        let dataSource = RxTableViewSectionedReloadDataSource<SectionModel<String,Movie>> {
+            _ ,tableview,indexPath, item in
+            let cell = tableview.dequeueReusableCell(withIdentifier: MainMovieCell.identifier,
+                                                     for: indexPath) as! MainMovieCell
+            let data = MovieTableCellViewModel(movieData: item)
+            cell.setupCell(viewModel: data)
             return cell
         } titleForHeaderInSection: { dataSource, sectionIndex in
             dataSource[sectionIndex].model
         }
+        
         /// Atrelando itens da tableview ao conjunto de objetos da viewModel
         self.viewModel.dataSource.bind(to: self.tableview.rx.items(dataSource: dataSource)).disposed(by: bag)
         
-        /// Monitoramento de ação de exclusão de itens na tableview
-//        tableview.rx.itemDeleted.subscribe(onNext: { [weak self] indexPath in
-            /// Ações quando houver um evento de exclusão
-//        }).disposed(by: bag)
-        
         /// Monitoramento de ação de seleção de itens na tableview
-//        tableview.rx.itemSelected.subscribe(onNext: { indexPath in
-            /// Ações quando houver uma seleção
-//        }).disposed(by: bag)
+        tableview.rx.modelSelected(Movie.self).subscribe(onNext: {
+            movie in
+            let data = DetailsMovieViewModel(movie: movie)
+            self.coordinator?.goToDetails(viewModel: data)
+        })
+        .disposed(by: bag)
     }
 }
